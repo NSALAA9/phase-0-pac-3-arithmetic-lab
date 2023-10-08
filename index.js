a14c32e0bfb0d4fe52bb5afc87138d29df6bc423
NSALAA9 <alaansairat99@gmail.com>
2023-10-08 20:30:59 +0000

    Completed

---
+++ index.js
@@ -0,0 +1,41 @@
+// Define the 'add' function
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Define the 'subtract' function
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Define the 'multiply' function
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Define the 'divide' function
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // Define the 'increment' function
+  function increment(n) {
+    return n + 1;
+  }
+  
+  // Define the 'decrement' function
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Define the 'makeInt' function
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  // Define the 'preserveDecimal' function
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+  
